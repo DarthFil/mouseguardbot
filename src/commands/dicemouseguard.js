b37f59789be5374f79033e0ef12cee1851a41a07
Felipe dos Santos Silva <felipesantos.net@LIVE.COM>
2020-03-30 23:22:29 -0300

    feat: Ajustes na imagens e melhorias no dados Mouse Guarde -Foi criado um canvas para exibir o resultado

--- src/commands/dicemouseguard.js
+++ src/commands/dicemouseguard.js
@@ -1,8 +1,18 @@
 const Discord = require('discord.js');
 const { DiceRoller } = require('rpg-dice-roller/lib/umd/bundle.js');
 const numero = require('numero-por-extenso');
+const Canvas = require('canvas');
 
-exports.run = (bot, msg, args) => {
+const applyText = (canvas, text) => {
+    const ctx = canvas.getContext('2d');
+    let fontSize = 60;
+    do {
+        ctx.font = `${fontSize -= 10}px Georgia, serif`;
+    } while (ctx.measureText(text).width > canvas.width - 300);
+    return ctx.font;
+};
+
+exports.run = async (bot, msg, args) => {
 
     let result = args[0] ? args[0] : 1;
     numberResult = Number.parseInt(result)
@@ -24,11 +34,40 @@ exports.run = (bot, msg, args) => {
         if (roll > 3 && roll <= 5) {
             success++;
         }
-        if (roll.total === 6) {
+        if (roll == 6) {
             decisive++;
         }
     }
-    msg.reply(`**${numberResult}** dados jogados \nCobra: **${failure}** \nEspada: **${success}** \nMachado: **${decisive}**`);
+
+    const canvas = Canvas.createCanvas(500, 250);
+    const ctx = canvas.getContext('2d');
+    const background = await Canvas.loadImage('./src/assets/imagens/texture/oldpaper.jpg');
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
+
+    ctx.strokeStyle = '#74037b';
+    ctx.font = applyText(canvas, "Teste")
+    ctx.fillStyle = '#2f3238';
+    ctx.fillText(`${failure}`, 110, 65);
+    ctx.fillText(`${success}`, 110, 140);
+    ctx.fillText(`${decisive}`, 110, 220);
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+
+    const snake = await Canvas.loadImage('./src/assets/imagens/icon/snake.png');
+    ctx.drawImage(snake,  25, 15, 75, 75);
+
+    const sword = await Canvas.loadImage('./src/assets/imagens/icon/sword.png');
+    ctx.drawImage(sword, 25, 90, 75, 75);
+
+    const axe = await Canvas.loadImage('./src/assets/imagens/icon/axe.png');
+    ctx.drawImage(axe, 25, 165, 75, 75);
+
+    const character = await Canvas.loadImage('./src/assets/imagens/character/Character.png');
+    ctx.drawImage(character, 235, 15, 160, 215);
+
+    const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'oldpaper.png');
+
+    msg.reply(`**${numberResult}** dados jogados`, attachment);
+    //msg.reply(`**${numberResult}** dados jogados \nCobra: **${failure}** \nEspada: **${success}** \nMachado: **${decisive}**`);
 
 };
 
--- src/commands/image.js
+++ src/commands/image.js
@@ -4,17 +4,31 @@ const config = require("../config/images.json")
 let text = "";
 
 exports.run = (bot, msg, args) => {
-    if (args == 'list') {
-        for (let i = 0; i < config.imagens.length; i++) {
-            text += `Nome: **${config.imagens[i].name}** \nDescrição: **${config.imagens[i].description}** \n`;
+
+    console.log(args);
+    let parameter1 = args[0] ? args[0] : "list";
+
+
+    if (args[0] == 'list') {
+        text += `**Cidades:** *(comando **!i t** + **Nome**)*\n`
+        for (let i = 0; i < config.city.length; i++) {
+            text += `Nome: **${config.city[i].name}** \nDescrição: **${config.city[i].description}** \n`;
+        }
+        text += `**Personagens:** *(comando **!i p** + **Nome**)*\n`
+        for (let i = 0; i < config.character.length; i++) {
+            text += `Nome: **${config.character[i].name}** \nDescrição: **${config.character[i].description}** \n`;
+        }
+        text += `**Mapas:** *(comando **!i m** + **Nome**)*\n`
+        for (let i = 0; i < config.map.length; i++) {
+            text += `Nome: **${config.map[i].name}** \nDescrição: **${config.map[i].description}** \n`;
         }
         msg.channel.send(text)
         return;
     }
     else {
         const image = new Discord.MessageEmbed()
-            .attachFiles([`./src/assets/${args}.jpg`])
-            .setImage(`attachment://${args}.jpg`);
+            .attachFiles([`./src/assets/imagens/${args[0]}/${args[1]}.jpg`])
+            .setImage(`attachment://${args[0]}.jpg`);
 
         msg.channel.send(image)
     }
